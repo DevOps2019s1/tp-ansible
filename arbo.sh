a2e296b99f48849a486804c83996090f9cebf0d5
Ninjasimov <lafayettelothaire@yahoo.fr>
2019-04-30 11:52:01 +0200

    add script sh arbo

---
+++ arbo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mkdir inventories inventories/production inventories/production/host_vars inventories/production/group_vars staging/ staging/group_vars staging/host_vars library module_utils filter_plugins roles roles/common roles/webtier roles/monitoring roles/apache
