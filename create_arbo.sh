53a075d08cd3ae72d0e771641a11d12834530c62
GregLebreton <greg.lebreton@hotmail.com>
2019-04-30 15:26:21 +0200

    push role apache

---
+++ create_arbo.sh
@@ -0,0 +1,1 @@
+mkdir -p inventories inventories/PROD inventories/PROD/host_vars inventories/PROD/group_vars inventories/DEV/ inventories/DEV/group_vars inventories/DEV/host_vars library module_utils filter_plugins roles roles/common roles/webtier roles/monitoring roles/
